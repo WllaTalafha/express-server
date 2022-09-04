c9dbd37d7c0a3cec6e2c152c3f190f56f3eff6bc
WllaTalafha <wlla.issa396@gmail.com>
2022-09-04 16:50:07 +0300

    test is done

---
+++ __test__/server.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const {app} = require('../server');
+
+const supertest = require('supertest');
+
+const request = supertest(app); 
+
+describe ('testing API server',()=>{
+    it('testing main route',async()=>{
+        const response = await request.get('/');
+        expect(response.text).toEqual('Hello World');
+        
+    })
+    it('testing /person route', async()=>{
+        const response = await request.get('/person');
+        expect(typeof response.body).toEqual('object');
+    })
+})
---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express');
+const app=express();
+
+app.get('/',(req,res)=>res.send('Hello World'));
+
+app.get('/person',(req,res)=>{
+    const {name,age,gender}=req.query;
+    let newAge=parseInt(age)+5;
+    res.send({name:name , gender:gender , age:`after 5 years you will be ${newAge}`})
+})
+
+function start (port){
+    app.listen(port,()=>console.log(`server is running on port ${port}`))
+}
+module.exports={app,start}
